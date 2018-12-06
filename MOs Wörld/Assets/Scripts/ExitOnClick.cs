4d2c7beda8d3761e22d4355b4aa2740ee09968cc
MaxiMalCore <45487640+MaxiMalCore@users.noreply.github.com>
2018-12-06 12:54:38 +0100

    rudimentäres Hauptmenü implementiert
    Start Button startet Level 1
    Exit Button beendet Spiel
    Shop Button geht noch nicht

---
+++ MOs Wörld/Assets/Scripts/ExitOnClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitOnClick : MonoBehaviour {
+
+	public void Quit()
+  {
+    Debug.Log("Button Geklickt und SPiel verlassen");
+
+    if (UnityEditor.EditorApplication.isPlaying == true)
+    {
+      UnityEditor.EditorApplication.isPlaying = false;
+    }
+    else
+    {
+      Application.Quit();
+    }
+  }
+}
