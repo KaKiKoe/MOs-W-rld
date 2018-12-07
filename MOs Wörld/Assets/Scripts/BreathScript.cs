7d40c44ac68d8d93146095c9ae60f89ee8397bef
MaxiMalCore <45487640+MaxiMalCore@users.noreply.github.com>
2018-12-07 18:10:31 +0100

    Luftanzeige implementiert

---
+++ MOs Wörld/Assets/Scripts/BreathScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class BreathScript : MonoBehaviour
+{
+
+  public Slider breathBarSlider;  
+  private Rigidbody2D rb;
+
+  void Start()
+  {
+    rb = GetComponent<Rigidbody2D>();
+    breathBarSlider.value = 100;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    PositionMo();
+  }
+
+  void PositionMo()
+  {
+    if (breathBarSlider.value >= .001f)
+    {
+      if (rb.position.y < -6)
+      {
+        breathBarSlider.value -= .005f;
+      }
+      else
+      {
+        breathBarSlider.value += .005f;
+      }
+    }
+    else 
+    {
+      SceneManager.LoadScene("End_fail");
+    }
+  }
+}
